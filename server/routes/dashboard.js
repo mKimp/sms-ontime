9075ca6b44aa19ff9338d3749aaa6f390c555d61
Kim Ma <phuongkim.ma@gmail.com>
2021-07-06 09:57:50 -0700

    Add the private contacts route for each user

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -2,17 +2,66 @@ const router = require("express").Router();
 const authorization = require("../middleware/authorization");
 const pool = require("../database/dbconfig");
 
+// all phone contacts
 router.get("/", authorization, async (req, res) => {
   try {
     const userId = req.user;
-    const userName = await pool.query(
-      "SELECT * FROM users WHERE user_id = $1",
+    const user = await pool.query(
+      "SELECT u.user_name, u.phone_number, c.contact_name, c.contact_number FROM users AS u LEFT JOIN user_contacts AS c ON u.user_id = c.user_id WHERE u.user_id = $1",
       [userId]
     );
-    res.json({
-      name: userName.rows[0].user_name,
-      phoneNumber: userName.rows[0].phone_number,
-    });
+    res.json(user.rows);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+//create a contact
+router.post("/contacts", authorization, async (req, res) => {
+  try {
+    const { contact_name, contact_number } = req.body;
+    const newUser = await pool.query(
+      "INSERT INTO user_contacts (user_id, contact_name, contact_number) VALUES ($1, $2, $3) RETURNING *",
+      [req.user, contact_name, contact_number]
+    );
+    res.json(newUser.rows[0]);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+//update a contact
+router.put("/contacts/:id", authorization, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { contact_name, contact_number } = req.body;
+    const updateContact = await pool.query(
+      "UPDATE user_contacts SET contact_name = $1, contact_number = $2 WHERE phone_id = $3 AND user_id = $4 RETURNING * ",
+      [contact_name, contact_number, id, req.user]
+    );
+
+    if (updateContact.rows.length == 0) {
+      return res.json("Not your contact");
+    }
+    res.json(updateContact.rows[0]);
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
+});
+
+//delete a contact
+router.delete("/contacts/:id", authorization, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateContact = await pool.query(
+      "DELETE FROM user_contacts WHERE phone_id = $1 AND user_id = $2 RETURNING *",
+      [id, req.user]
+    );
+
+    if (updateContact.rows.length == 0) {
+      return res.json("Can't delete the contact");
+    }
+    res.json(updateContact.rows[0]);
   } catch (error) {
     res.status(500).send("Server error");
   }
